a95770f69f6ae60c1e8d77e3fa5496ff5c17dc52
Joseph Soria <josephsoria6@gmail.com>
2021-03-13 22:15:02 -0600

    Updated PUT request

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -106,7 +106,6 @@ router.put('/:id', (req, res) => {
     })
     .then((updatedProductTags) => res.json(updatedProductTags))
     .catch((err) => {
-      // console.log(err);
       res.status(400).json(err);
     });
 });
